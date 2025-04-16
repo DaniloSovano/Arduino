9817b09fcdc01e5c930c81053646fe92fb754fa0
Danilo Sovanogit config --global user.mail danilosovano1@gmail.com <danilosovano1@gmail.com>
2025-04-16 13:40:33 -0300

    WIP: mudanças locais antes do rebase

--- ds18b20_MQTT_sensor_testes/ds18b20_MQTT_sensor_testes.ino
+++ ds18b20_MQTT_sensor_testes/ds18b20_MQTT_sensor_testes.ino
@@ -18,6 +18,7 @@
 #include <DallasTemperature.h>
 #include <WebServer.h>
 #include <Update.h>
+#include "htmlPage.h"
 
 // *********************************************** ALIAS ***************************************************
 using charArray = std::shared_ptr<char[]>;
@@ -31,66 +32,6 @@ int DELAY = 0;
 char delayStr[10];
 
 
-const char serverIndex[] PROGMEM = R"(
-<!DOCTYPE HTML>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <title>Atualização de Firmware</title>
-    <style>
-        body {
-            background-color: #111111;
-            font-family: Arial, sans-serif;
-            text-align: center;
-            padding: 60px;
-        }
-        h1 {
-            color: #9966CB;
-            font-size: 50px;
-        }
-        h2 {
-            color: white;
-            font-size: 30px;
-        }
-        p {
-            font-size: 45px;
-            color: #cccccc;
-            font-weight: bold;
-        }
-        span {
-            font-size: 40px;
-            color: #cccccc;
-        }
-        input[type="file"] {
-            margin-top: 20px;
-            font-size: 18px;
-            color: white;
-        }
-        input[type="submit"] {
-            background-color: #9966CB; 
-            color: white;
-            border: none;
-            padding: 10px 20px;
-            font-size: 18px;
-            cursor: pointer;
-            border-radius: 5px;
-            margin-top: 10px;
-        }
-        input[type="submit"]:hover {
-            background-color: #7748A6; 
-        }
-    </style>
-</head>
-<body>
-    <h1>Atualização de Firmware</h1>
-    <h2>Escolha um arquivo .bin</h2>
-    <form method="POST" action="/update" enctype="multipart/form-data">
-        <input type="file" name="update">
-        <input type="submit" value="Atualizar Firmware">
-    </form>
-</body>
-</html>
-)";
 const char* mqttClientId = "esp8266_01";
 const char* TopicReset = "esp8266_01/reset";
 const int ONE_WIRE_BUS = 4;
